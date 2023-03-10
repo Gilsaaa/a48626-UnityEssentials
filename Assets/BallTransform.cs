8ce3cad8ed051cab98d3d65c35ba07ab8554576a
Gilsaaa <jegilsa34@gmail.com>
2023-03-10 10:11:25 +0000

    Change a GameObject with script

--- Assets/BallTransform.cs
+++ Assets/BallTransform.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class News : MonoBehaviour
+public class BallTransform : MonoBehaviour
 {
+    public Vector3 scaleChange;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class News : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale += scaleChange;
     }
 }
